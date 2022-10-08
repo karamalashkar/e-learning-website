a4b90fd181a4f344ca07843b381500d7d2c22ed1
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-09 00:27:45 +0300

    Backend <> Adding add new assignment api

--- backend/elearning_server/app/Http/Controllers/InstructorController.php
+++ backend/elearning_server/app/Http/Controllers/InstructorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Enroll;
+use App\Models\Assignment;
 
 class InstructorController extends Controller
 {
@@ -37,4 +38,37 @@ class InstructorController extends Controller
         ]);
 
     }
+
+    //add new assignment
+    function addAssignment(Request $request){
+        $base64Image = explode(";base64,", $request->attachement);
+        $explodeImage = explode("image/", $base64Image[0]);
+        $imageType = $explodeImage[1];
+        $image_base64 = base64_decode($base64Image[1]);
+        $imageName = $request->ccode.'_'.$request->title.'.png';
+        \File::put(public_path(). '/assignments/' . $imageName, base64_decode($base64Image[1]));
+        
+        
+        $assignment= new Assignment;
+
+        $assignment->course_code=$request->ccode;
+        $assignment->title=$request->title;
+        $assignment->body=$request->body;
+        $assignment->attachement=$imageName;
+        $assignment->due=$request->due;
+        
+        if($assignment->save()){
+            return response()->json([
+                'status' => "success",
+                'data' => $assignment
+            ]);
+        }
+
+        return response()->json([
+            'status' => "failed",
+            'data' =>'error'
+        ]);
+
+    }
+
 }
--- backend/elearning_server/routes/api.php
+++ backend/elearning_server/routes/api.php
@@ -9,6 +9,7 @@ Route::group(['prefix'=>"v1"],function(){
     Route::post('/add_course',[CourseController::class,'addCourse']);
     Route::post('/assign_instructor',[CourseController::class,'assignInstructor']);
     Route::post('/enroll_student',[InstructorController::class,'enrollStudent']);
+    Route::post('/add_assignment',[InstructorController::class,'addAssignment']);
     Route::post('/add',[UserController::class,'addUser']);
 });
 

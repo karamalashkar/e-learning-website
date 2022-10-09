7bf9c423bee58c977206dd686750b7720086dca9
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-09 12:08:58 +0300

    Frontend <> Adding navbar

---
+++ frontend/e-learning-application/src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import { useRef } from "react";
+import "../styles/style.css";
+
+function Navbar() {
+	
+	return (
+		<header>
+			<h1>E-learning</h1>
+			<nav>
+				<a href="/#">Courses</a>
+				<a href="/#">Instructors</a>
+				<a href="/#">Students</a>
+                <button className="logout">
+                    Logout
+                </button>
+			</nav>
+		</header>
+	);
+}
+
+export default Navbar;

5abac70720f6b19c6997f9c7cb03ef301fd8b090
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-09 12:33:42 +0300

    Frontend <> Adding sidebar

--- frontend/e-learning-application/src/App.js
+++ frontend/e-learning-application/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
 	return (
 		<React.Fragment>
 			<Navbar/>
+      <Sidebar/>
 		</React.Fragment>
 	);
 }
---
+++ frontend/e-learning-application/src/Components/Sidebar.js
@@ -0,0 +1,16 @@
+import { useRef } from "react";
+import "../styles/style.css";
+
+function Navbar() {
+	
+	return (
+		<div className="sidebar flex">
+			<h1 className="title">Unassigned Courses</h1>
+            <div className="courses flex">
+
+            </div>
+		</div>
+	);
+}
+
+export default Navbar;
